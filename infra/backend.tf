2df4f61318299798a9d188e0420d506084feb760
Mohamed Emad <153327897+Mohamed0emad@users.noreply.github.com>
2026-02-08 00:45:19 +0200

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "reddit-nti-tfstate"
-    key            = "terraform.tfstate"
+    bucket         = "reddit-nti-tfstate01"
+    key            = "terraform.tfstate01"
     region         = "us-east-1"
 
     dynamodb_table = "reddit-nti-tfstate-lock"
